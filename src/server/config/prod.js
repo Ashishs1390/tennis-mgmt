f6656cb334062630f035f5c4692aa511f28e1573
Ashishs1390 <ashishhsharma1990@gmail.com>
2021-12-14 13:43:58 +0530

    f

--- src/server/config/prod.js
+++ src/server/config/prod.js
@@ -1,4 +1,4 @@
-export const config ={
+const config ={
     dbUrl: `mongodb+srv://ashishs104:Arsenal@1886@cluster0.tr5qs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 }
 module.exports = config;
