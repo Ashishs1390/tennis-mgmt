2847d7949308878192d189565f4ebefdde7ae17b
Ashishs1390 <ashishhsharma1990@gmail.com>
2022-01-15 01:23:43 +0530

    apis

--- src/server/api/linktoplayer/linkToPlayerCtrl.js
+++ src/server/api/linktoplayer/linkToPlayerCtrl.js
@@ -7,6 +7,7 @@ router.route('/').put(async (req, res, next) => {
         const isExist = await basicInformation.find({ email: player_email }).catch((err) => {
             console.log(err);
         })
+        let updatedInfo;
         let updatePlayerObj = {};
         let updatePCObj = {};
         console.log(isExist);
@@ -24,13 +25,15 @@ router.route('/').put(async (req, res, next) => {
             updatePlayerObj.coach_email = coach_email;
             updatePCObj.email = coach_email;
         }
-        const updatedPlayerInfo = await basicInformation.updateOne(
+        console.log(updatePlayerObj)
+        const updatedPlayerInfo = await basicInformation.findOneAndUpdate(
             { email: player_email }, {
-            $set: {
+            $addToSet: {
                 ...updatePlayerObj
             }
         }, {
-            upsert: true
+            upsert: true,
+            returnOriginal: false
         }).catch((err) => {
             console.log(err);
             res.status(504).json({
@@ -39,13 +42,14 @@ router.route('/').put(async (req, res, next) => {
             })
         })
         if (updatedPlayerInfo) {
-            await basicInformation.updateOne(
+            updatedInfo = await basicInformation.findOneAndUpdate(
                 { ...updatePCObj }, {
-                $set: {
+                $addToSet: {
                     children_email: player_email
                 }
             }, {
-                upsert: true
+                upsert: true,
+                returnOriginal: false
             }).catch((err) => {
                 console.log(err);
                 res.status(504).json({
@@ -55,20 +59,24 @@ router.route('/').put(async (req, res, next) => {
             })
 
         }
+        console.log("----------------updatedInfo--------updatedPlayerInfo----------------------")
+        console.log(updatedInfo);
+        console.log(updatedPlayerInfo);
         res.status(200).send({
             msg: "emails updated successfully",
+            children_email: updatedInfo.children_email,
             status: 200
         });
-        res.send({ a: 10 });
     }
     catch (err) {
         console.log(err);
+        res.status(504).json({
+            errMsg: "internal server error",
+            status: 504
+        })
     }
 });
-
-router.route('/').get(async (req, res, next) => {
-    console.log(req.user[0]);
-    const { email } = req.user[0];
+const getChildrenList = async(email) => {
     let childList = await basicInformation.find({ email: email }, { children_email: 1, _id: 0 }).catch((err) => {
         console.log(err);
     })
@@ -77,13 +85,36 @@ router.route('/').get(async (req, res, next) => {
     childlist = childList[0]
 
     if (childList.length !== 0) {
-        res.send(childList[0])
-    } else {
-        res.status(404).send({
-            errorMsg: "player not found",
-            code: 404
-        })
+        return (childList[0])
     }
+}
+const getSearchedChild = async ({email }) => {
+    let output = await basicInformation.find({ email: email }, { email:1,_id:0})
+    return output;
+}
+
+router.route('/').get(async (req, res, next) => {
+    console.log(req.query)
+    try {
+        console.log(req.user[0]);
+        const { email } = req.user[0];
+
+
+        if (req.query && Object.keys(req.query).length === 0
+            && Object.getPrototypeOf(req.query) === Object.prototype) {
+            console.log("0000")
+            const resp = await getChildrenList(email);
+            res.status(200).send(resp)
+
+        } else {
+            const resp = await getSearchedChild(req.query);
+            console.log(resp)
+            res.status(200).send(resp)
+        }
+    } catch (err) {
+        console.log(err);
+    }
+    
 })
 
 module.exports = router;
