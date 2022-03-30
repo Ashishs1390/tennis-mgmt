23fa357406a48973c4701b23c52815ecd1fb6212
Ashishs1390 <ashishhsharma1990@gmail.com>
2022-03-30 21:46:00 +0530

    up heroku

--- src/client/components/player-coach/LandingPage/LandingPage.js
+++ src/client/components/player-coach/LandingPage/LandingPage.js
@@ -151,6 +151,7 @@ function LandingPage(props) {
           onClick={() => {
             updateNav('./assessments');
           }}
+          
         >
           Assessments
         </MenuItem>
