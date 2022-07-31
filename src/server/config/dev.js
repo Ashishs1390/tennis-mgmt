32afe4eb86316e17973b081fd0fdf83ae566b7d5
Ashishs1390 <ashishhsharma1990@gmail.com>
2022-07-31 22:51:27 +0530

    video amalysis chaanges

--- src/server/config/dev.js
+++ src/server/config/dev.js
@@ -1,6 +1,6 @@
 const config = {
-    // dbUrl: `mongodb+srv://ashishs104:Arsenal%401886@cluster0.tr5qs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-    dbUrl: `${process.env.MONGO_URI}`
+    dbUrl: `mongodb+srv://ashishs104:Arsenal%401886@cluster0.tr5qs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    // dbUrl: `${process.env.MONGO_URI}`
 }
 
 module.exports = config;
